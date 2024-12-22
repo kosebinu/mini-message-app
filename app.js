12734fcc31ca937b680b8036f4a62ed3607805a0
Kazeem Kosebinu <138680238+kosebinu@users.noreply.github.com>
2024-12-21 20:26:56 -0500

    Fix dependencies

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/message/:id', (req, res) => {
   res.render('message', { title: 'Message Detail', message });
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
